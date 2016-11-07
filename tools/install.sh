4fa5d7cc51e613aee2cabbac447582f808fdeaac
Bryan Tidwell <btidwell4@my.apsu.edu>
2016-11-06 20:06:24 -0600

    misspelled my name :P

--- tools/install.sh
+++ tools/install.sh
@@ -61,7 +61,7 @@ main() {
       exit 1
     fi
   fi
-  env git clone --depth=1 https://github.com/DwordPtrl/oh-my-zsh.git $ZSH || {
+  env git clone --depth=1 https://github.com/DwordPtr/oh-my-zsh.git $ZSH || {
     printf "Error: git clone of oh-my-zsh repo failed\n"
     exit 1
   }
